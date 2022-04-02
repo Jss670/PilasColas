ce43bc345056a20782ae50fec5925920042a7532
Octavio Garcia <jss670@gmail.com>
2022-04-02 12:49:10 -0700

    lista colas

--- Listas/Colas.cs
+++ Listas/Colas.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Listas
 {
-    internal class Colas
+    public class Colas
     {
+        private List<string> lista;
+        public Colas()
+        {
+            lista = new List<string>();
+        }
+        public void Agregar(string dato)
+        {
+            lista.Add(dato);
+
+        }
+        private bool ValidarVacio()
+        {
+            return lista.Count > 0;
+        }
+        public void Eliminar()
+        {
+            if (ValidarVacio())
+            {
+                throw new Exception("Lista Vacia");
+            }
+            lista.RemoveAt(0);
+        }
+        public string Imprimir()
+        {
+            string datos = "";
+
+            if (ValidarVacio())
+            {
+                return "Lista Vacia";
+            }
+            int lstCount = lista.Count;
+            for (int i = 0; i < lstCount; i++)
+            {
+
+            }
+            return datos;
+        }
     }
 }
+
