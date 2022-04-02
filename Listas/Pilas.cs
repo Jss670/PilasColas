69a4619353b39864cfab4111a8cf168f7c37c807
Octavio Garcia <jss670@gmail.com>
2022-04-02 13:36:59 -0700

    pilas lista actualizada

--- Listas/Pilas.cs
+++ Listas/Pilas.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace Listas
 {
-    internal class Pilas
+    public class Pilas
     {
+        private List<string> list;
+        public Pilas()
+        {
+            list = new List<string>();
+        }
+        public void Agregar(string dato)
+        {
+            list.Add(dato);
+        }
+        private bool ValidaVacio()
+        {
+            return list.Count > 0;
+        }
+        public void Eliminar()
+        {
+            if (ValidaVacio())
+            {
+                throw new Exception("Lista Vacia");
+            }
+            list.RemoveAt(list.Count-1);
+        }
+        public string Imprimir()
+        {
+            string datos = "";
+
+            if (ValidaVacio())
+            {
+                return "Lista Vacia";
+            }
+            int lstCount = lista.Count;
+            for (int i = 0; i < lstCount; i++)
+            {
+                if (i > 0)
+                {
+                    datos = "\n";
+                }
+                datos += "$[{ i}]-{ lista[{ i}]}";
+
+            }
+            return datos;
+        }
     }
 }
